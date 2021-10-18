4575af5fd6af2285f3e381c6b8cf767fb117a29b
sangwookShin <shinsw6455@naver.com>
2021-10-18 22:20:33 +0900

    프로그래머스 JadenCase 문자열 만들기

---
+++ Programmers/Programmers/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string s) {
+
+    s[0] = toupper(s[0]);
+
+    for (int i = 1; i < s.size(); i++) {
+
+        if (isalpha(s[i]) && s[i - 1] == ' ') {
+            s[i] = toupper(s[i]);
+        }
+        else {
+            s[i] = tolower(s[i]);
+        }
+    }
+
+    return s;
+}
