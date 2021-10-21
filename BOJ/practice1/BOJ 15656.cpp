aac743d1b09204f9db9d36bb0d856356fe4b5921
sangwookShin <shinsw6455@naver.com>
2021-10-21 22:13:18 +0900

    BOJ 15656 백트래킹

---
+++ BOJ/practice1/BOJ 15656.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[8];
+bool visit[8];
+vector<int> v;
+
+void DFS(int cnt) {
+
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < v.size(); i++) {
+
+
+        visit[i] = true;
+        arr[cnt] = v[i];
+        DFS(cnt + 1);
+        visit[i] = false;
+    }
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    sort(v.begin(), v.end());
+
+    DFS(0);
+
+    return 0;
+}
