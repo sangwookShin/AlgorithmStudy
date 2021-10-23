920504052e153f63f8de864be45fb6d40ef0a348
sangwookShin <shinsw6455@naver.com>
2021-10-23 23:33:41 +0900

    프로그래머스 위장

---
+++ Programmers/Programmers/위장.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> m;
+
+    map<string, int>::iterator iter;
+
+    for (int i = 0; i < clothes.size(); i++) {
+        m[clothes[i][1]]++;
+    }
+
+    for (iter = m.begin(); iter != m.end(); iter++) {
+        answer = answer * (iter->second + 1);
+    }
+
+    return answer - 1;
+}
