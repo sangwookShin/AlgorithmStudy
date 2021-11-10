e36e468c111126418012f5213a3060071692a26a
sangwookShin <shinsw6455@naver.com>
2021-11-10 23:18:46 +0900

    BOJ 1927 우선순위큐

---
+++ BOJ/practice1/BOJ 1927.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N;
+void solve() {
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < N; i++) {
+
+        int a;
+        cin >> a;
+        if (a == 0) {
+            if (pq.empty()) {
+                cout << "0" << "\n";
+            }
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(a);
+        }
+    }
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+    solve();
+
+    return 0;
+}
